0b6de0d7cab8bcfdb44b6396172268520d132755
Michael Kelsey <kelsey@slac.stanford.edu>
2015-11-10 15:19:56 -0800

    New executable to exercise only queries from XRootD (servers must be launched manually).

---
+++ query-xrd.cc
@@ -0,0 +1,25 @@
+#include "XrootdSimple.hh"
+#include <stdlib.h>
+#include <iostream>
+
+
+// Get command line arguments for array size (100M) and number of trials (1M)
+void arrayArgs(int argc, char* argv[], unsigned long long& asize, int& reps) {
+  asize = 0;
+  reps  = (argc>1) ? strtol(argv[2], 0, 0)   : 1000000;
+}
+
+
+// Main program goes here
+
+int main(int argc, char* argv[]) {
+  unsigned long long arraySize;
+  int queryTrials;
+  arrayArgs(argc, argv, arraySize, queryTrials);
+
+  std::cout << "XRootD Table query " << queryTrials
+	    << " trials" << std::endl;
+
+  XrootdSimple xrdidx(2);		// Verbosity
+  xrdidx.ExerciseTable(queryTrials);
+}
