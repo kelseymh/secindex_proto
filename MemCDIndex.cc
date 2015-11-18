485bdc9e91f9be4f782978cde84bdbc53bbf4948
Michael Kelsey <kelsey@slac.stanford.edu>
2015-11-18 12:29:39 -0800

    Add diagnostic messages for memcached transactions.

--- MemCDIndex.cc
+++ MemCDIndex.cc
@@ -3,6 +3,7 @@
 //
 // 20151023  Michael Kelsey
 // 20151104  Bug fix:  Must terminate execvp() arg list with null pointer
+// 20151118  Add diagnostic messages for all memcached actions
 
 #include "MemCDIndex.hh"
 #include <libmemcached/memcached.h>
@@ -39,21 +40,22 @@ bool MemCDIndex::launchServer(unsigned long long asize) {
       cout << "Successfully forked child for " << needmem << " MB server"
 		<< " (PID " << mcdsv << ")" << endl;
     }
+    sleep(3);		// Wait for server to start
+    return true;
   } else if (mcdsv < 0) {
     cerr << "Server creation failed!" << endl;
     return false;
-  } else if (mcdsv == 0) {		// This is the child, start the server
-    // Argument to "-m" is total size in megabytes
-    char* args[] = { "memcached", "-m", 0, 0 };
-
-    int slen = snprintf(0,0,"%ld",needmem);	// Length of string
-    args[2] = (char*)malloc(slen+1);
-    snprintf(args[2],slen+1,"%ld",needmem);
-
-    if (verboseLevel>1) cout << "Launching server in child..." << endl;
-    execvp(args[0], args);			// This should never return!
-    ::exit(0);
   }
+
+  // Argument to "-m" is total size in megabytes
+  char* args[] = { "memcached", "-m", (char*)0, (char*)0 };
+  
+  int slen = snprintf(0,0,"%ld",needmem);	// Length of string
+  args[2] = (char*)malloc(slen+1);
+  snprintf(args[2],slen+1,"%ld",needmem);
+  
+  execvp(args[0], args);			// This should never return!
+  ::exit(0);
 
   return true;
 }
@@ -69,15 +71,25 @@ bool MemCDIndex::launchClient(unsigned long long asize) {
   }
 
   stringstream mcdstr;
-  mcdstr << "--SERVER=localhost" << " --POOL-MAX=" << asize;
+  mcdstr << "--SERVER=localhost --BINARY-PROTOCOL --POOL-MAX=" << asize;
   const char* mcdconf = mcdstr.str().c_str();
+
   memcd = memcached(mcdconf, strlen(mcdconf));
   if (!memcd) {
     cerr << "Client creation failed!" << endl;
     return false;
   }
-  
-  if (verboseLevel>1) cout << "Successfully created client." << endl;
+
+  // Set non-blocking I/O for maximum performance
+  memcached_behavior_set(memcd, MEMCACHED_BEHAVIOR_NO_BLOCK, 1);
+
+  if (verboseLevel) {
+    memcached_return_t mcdret =
+      libmemcached_check_configuration(mcdconf, strlen(mcdconf), 0, 0);
+    cerr << memcached_strerror(memcd, mcdret) << endl;
+  }
+
+  if (verboseLevel>1) cout << "Successfully created client " << memcd << endl;
   return true;
 }
 
@@ -86,11 +98,18 @@ void MemCDIndex::create(unsigned long long asize) {
   
   launchServer(asize); if (!mcdsv) return;
   launchClient(asize); if (!memcd) return;
-  
+
+  if (verboseLevel>1) cout << "Filling " << asize << " keys" << endl;
+
+  memcached_return_t error;
   static const int zero=0;		// All keys have same dummy value
   for (unsigned long long key=0; key<asize; key++) {
-    memcached_set(memcd, (const char*)&key, sizeof(key), (const char*)&zero,
-		  sizeof(int), 0, 0);
+    error = memcached_set(memcd, (const char*)&key, sizeof(key),
+			  (const char*)&zero, sizeof(int), 0, 0);
+    if (error != MEMCACHED_SUCCESS) {
+      cerr << "Failed to store key " << key << ": " 
+	   << memcached_strerror(memcd,error) << endl;
+    }
   }
 }
 
@@ -100,17 +119,24 @@ void MemCDIndex::create(unsigned long long asize) {
 int MemCDIndex::value(unsigned long long index) {
   if (!memcd) return 0xdeadbeef;	// Include sanity check
 
+  if (verboseLevel>1) cout << "Looking for " << index;
+
   size_t blen;
   uint32_t flags;
   memcached_return_t error;
   char* buf = memcached_get(memcd, (const char*)&index, sizeof(index),
 			    &blen, &flags, &error);
+
+  if (verboseLevel>1 && error != MEMCACHED_SUCCESS)
+    cerr << "memcached error " << memcached_strerror(memcd,error) << endl;
+			   
   if (!buf) return 0xdeadbeef;
-  
+
   int valbuf;			// To copy "byte array" into numeric value
   memcpy(&valbuf, buf, blen);
   delete buf;			// Avoid memory leaks
-  
+
+  if (verboseLevel>1) cout << " got value " << valbuf << endl;
   return valbuf;
 }
 
